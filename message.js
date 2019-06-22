4bbc5ffcc9d44112d7ac7c6cbf44c0a0e00d6b5e
Cooper Barth <cooperfbarth@gmail.com>
2019-06-21 20:49:30 -0500

    markdown edits

--- message.js
+++ message.js
@@ -1,12 +1,13 @@
-const responseMarkdown = (channelJSON) => {
-    if (!channelJSON.ok) {
-        return `Something went wrong (${channelJSON.err}). Try again?`;
+const responseMarkdown = (messageJSON) => {
+    if (!messageJSON.ok) {
+        return `Something went wrong (${messageJSON.err}). Try again?`;
     }
-    channels = channelJSON.channels;
+    const channels = messageJSON.channels;
+    const phrase = messageJSON.phrase;
     if (channels.length === 0) {
-        return `No channels found for query '${channelJSON.phrase}'.`;
+        return `No channels found for query '${phrase}'.`;
     } else {
-        let returnString = ``;
+        let returnString = `__Mentions__ of _"${phrase}"_:`;
         for (let i = 0; i < Math.min(channels.length, 4); i++) {
             const channel = channels[i];
             returnString += `*<#${channel.id}|${channel.name}>:* ${channel.count} mentions.\n`;
