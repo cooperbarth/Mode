04df0dfc76bce198b7c5a8eaba54082f7e7599c1
Cooper Barth <cooperfbarth@gmail.com>
2019-06-21 20:28:01 -0500

    syntax

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const request = require('request');
 
-const message = require('message');
+const message = require('./message.js');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
