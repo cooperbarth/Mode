2715599dbe85b5b5d04fa0f132f7bf6bf4a8be36
Cooper Barth <cooperfbarth@gmail.com>
2019-06-22 00:00:57 -0500

    added expert route

--- src/channelResponse.js
+++ src/channelResponse.js
@@ -1,4 +1,7 @@
-const responseMarkdown = (messageJSON) => {
+const MAX_CHANNELS = 5;
+const TIMEOUT_MESSAGE = ' (Not all channels were searched due to a timeout.)';
+
+const responseMarkdown = (ok, err, phrase, data) => {
     if (!messageJSON.ok) {
         return `Something went wrong (${messageJSON.err}). Try again?`;
     }
@@ -7,8 +10,8 @@ const responseMarkdown = (messageJSON) => {
     if (channels.length === 0) {
         return `Sorry! I couldn't find any mentions of "${phrase}".`;
     } else {
-        let returnString = `*Channels that mention "${phrase}":*${(messageJSON.finished)? '' : ' (Not all channels were searched due to a timeout.)'}\n`;
-        for (let i = 0; i < Math.min(channels.length, 4); i++) {
+        let returnString = `*Channels that mention "${phrase}":*${(messageJSON.finished)? '' : TIMEOUT_MESSAGE}\n`;
+        for (let i = 0; i < Math.min(channels.length, MAX_CHANNELS); i++) {
             const channel = channels[i];
             const count = channel.count;
             returnString += `*<#${channel.id}|${channel.name}>:* ${count} mention${(count === 1)? '' : 's'}\n`;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const request = require('request');
 
-const message = require('./message.js');
+const channelResponse = require('./channelResponse');
+const userResponse = require('./userResponse');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -16,39 +17,20 @@ console.log(`Server running on port ${serverPort}.`)
 
 app.post('/search', (req, res) => {
     const phrase = req.body.text;
-
-    const channelsUrl = `https://slack.com/api/conversations.list?token=${process.env.OAUTH_TOKEN}&limit=100&exclude_archived=true&types=public_channel`
+    const channelsUrl = `https://slack.com/api/conversations.list?token=${process.env.OAUTH_TOKEN}&limit=500&exclude_archived=true&types=public_channel`
     request(channelsUrl, (err, _, body) => {
         if (err) {
-            res.send(message({
-                ok: false,
-                err: err,
-                phrase: phrase,
-                channels: [],
-                finished: false
-            }));
+            res.send(channelResponse(false, err, phrase, []));
         }
         body = JSON.parse(body);
         if (!body.ok) {
-            res.send(message({
-                ok: false,
-                err: body.error,
-                phrase: phrase,
-                channels: [],
-                finished: false
-            }));
+            res.send(channelResponse(false, body.error, phrase, []));
         } else {
             const channels = body.channels;
             let orderedChannels = [];
             let seenChannels = 0;
-            const timeout = setTimeout(() => { //send whatever we have after 5 seconds
-                res.send(message({
-                    ok: true,
-                    err: "",
-                    phrase: phrase,
-                    channels: orderedChannels,
-                    finished: false
-                }));
+            const timeout = setTimeout(() => { //send whatever we have after 3 seconds
+                res.send(channelResponse(false, "Request timed out.", phrase, orderedChannels));
             }, 3000);
             for (let channel of channels) { //get the messages for each channel
                 const messagesUrl = `https://slack.com/api/channels.history?token=${process.env.OAUTH_TOKEN}&channel=${channel.id}&count=1000`;
@@ -64,26 +46,75 @@ app.post('/search', (req, res) => {
                                 }
                             }
                             if (count !== 0) {
-                                const channelJSON = {
+                                orderedChannels.push({
                                     id: channel.id,
                                     name: channel.name,
                                     count: count,
-                                };
-                                orderedChannels.push(channelJSON);
+                                });
                             }
-                            seenChannels++;
-                            if (seenChannels === channels.length) { //this is the last channel
+
+                            if (++seenChannels === channels.length) { //this is the last channel
                                 clearTimeout(timeout);
                                 orderedChannels.sort((c1, c2) => {
                                     return c1.count < c2.count;
                                 })
-                                res.send(message({
-                                    ok: true,
-                                    err: "",
-                                    phrase: phrase,
-                                    channels: orderedChannels,
-                                    finished: true
-                                }));
+                                res.send(channelResponse(true, "", phrase, orderedChannels));
+                            }
+                        }
+                    }
+                });
+            }
+        }
+    });
+});
+
+app.post('/experts', (req, res) => {
+    const phrase = req.body.text;
+    const channelsUrl = `https://slack.com/api/conversations.list?token=${process.env.OAUTH_TOKEN}&limit=500&exclude_archived=true&types=public_channel`
+    request(channelsUrl, (err, _, body) => {
+        if (err) {
+            res.send(userResponse(false, err, phrase, {}));
+        }
+        body = JSON.parse(body);
+        if (!body.ok) {
+            res.send(userResponse(false, body.error, phrase, {}));
+        } else {
+            const channels = body.channels;
+            let seenChannels = 0;
+            let users = {}; //maps username to # of messages
+            
+            const timeout = setTimeout(() => { //send whatever we have after 3 seconds
+                res.send(userResponse(false, "Request timed out.", phrase, users));
+            }, 3000);
+
+            for (let channel of channels) { //get the messages for each channel
+                const messagesUrl = `https://slack.com/api/channels.history?token=${process.env.OAUTH_TOKEN}&channel=${channel.id}&count=1000`;
+                request(messagesUrl, (err, _, body) => {
+                    if (!err) { //if this channel broke, we'll just discount the channel
+                        body = JSON.parse(body);
+                        if (body.ok) { //if not ok, we'll just discount the channel
+                            const messages = body.messages;
+                            for (let message of messages) {
+                                if (message.text.toLowerCase().includes(phrase.toLowerCase())) {
+                                    if (message.user in users) {
+                                        users.user = users.user + 1;
+                                    } else {
+                                        users.user = 1;
+                                    }
+                                }
+                            }
+                            
+                            if (++seenChannels === channels.length) { //this is the last channel
+                                clearTimeout(timeout);
+                                let keys = Object.keys(users);
+                                keys.sort((k1, k2) => {
+                                    return users[k1] < users[k2];
+                                });
+                                const MAX_USERS = 4;
+                                keys = keys.slice(0, MAX_USERS);
+                                users = keys.reduce((key, val) => (key[val] = users[val], key), {});
+
+                                res.send(userResponse(true, "", phrase, users));
                             }
                         }
                     }
