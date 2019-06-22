854d61f17a0b2c39f8e0fe6993d51870177f56c3
Cooper Barth <cooperfbarth@gmail.com>
2019-06-22 01:10:48 -0500

    testing

--- index.js
+++ index.js
@@ -99,11 +99,10 @@ app.post('/experts', (req, res) => {
                             for (let message of messages) {
                                 if (message.text.toLowerCase().includes(phrase.toLowerCase())) {
                                     const user = message.user;
-                                    console.log(message, user)
                                     if (user in users) {
-                                        users.user = users.user + 1;
+                                        users[user] = users[user] + 1;
                                     } else {
-                                        users.user = 1;
+                                        users[user] = 1;
                                     }
                                 }
                             }
