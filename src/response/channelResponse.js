c60a319a6e39175b52a146692b9f7fe91b296bc8
Cooper Barth <cooperfbarth@gmail.com>
2019-06-28 22:16:32 -0500

    fixup

--- src/response/channelResponse.js
+++ src/response/channelResponse.js
@@ -26,7 +26,7 @@ const responseMarkdown = (ok, err, phrase, channels) => {
         const otherChannels = channels.filter(channel => !supportServiceChannels.includes(channel));
         if (otherChannels.length !== 0) {
             returnString += `*${supportServiceChannels.length > 0 ? 'Other ' : ''}Channels that mention "${phrase}":*\n`;
-            for (let i = 0; i < Math.min(otherChannels.length, max(MAX_CHANNELS - supportServiceChannels.length, 0)); i++) {
+            for (let i = 0; i < Math.min(otherChannels.length, Math.max(MAX_CHANNELS - supportServiceChannels.length, 0)); i++) {
                 const channel = otherChannels[i];
                 const count = channel.count;
                 returnString += `*<#${channel.id}|${channel.name}>:* ${count} mention${(count === 1)? '' : 's'}\n`;
